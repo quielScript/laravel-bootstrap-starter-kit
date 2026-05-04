f0ebc6cd493bb1c39a7066c98e6350df7bb16f35
Exequiel A. <exequielarco23@gmail.com>
2026-05-04 10:04:36 +0800

    feat(auth): added laravel breeze authentication

---
+++ resources/views/components/responsive-nav-link.blade.php
@@ -0,0 +1,11 @@
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'd-block w-100 ps-3 pe-4 py-2 border-start border-5 border-primary text-start text-base fw-medium text-primary bg-light'
+            : 'd-block w-100 ps-3 pe-4 py-2 border-start border-5 border-transparent text-start text-base fw-medium text-secondary';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
